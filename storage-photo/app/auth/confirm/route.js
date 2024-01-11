b51d0ebb9a8c0ab52a36204a1cf8ce2312ce8512
Levon Aslanyan <aslanyan2012@mail.ru>
2024-01-12 01:14:58 +0400

    middleware

--- storage-photo/app/auth/confirm/route.js
+++ storage-photo/app/auth/confirm/route.js
@@ -1,41 +1,41 @@
 import { createServerClient } from "@supabase/ssr";
-import next from "next";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
-  const type = searchParams.get("next");
-  const cookiesStore = cookies();
+  const next = searchParams.get("next");
+  const type = searchParams.get("type");
+  const cookieStore = cookies();
 
-  const supabse = createServerClient(
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name) {
-          return cookies.get(name)?.value;
+          return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          cookiesStore.set({ name, value, ...options });
+          cookieStore.set({ name, value, ...options });
         },
         remove(name, options) {
-          cookiesStore.set({ name, value: "", ...options });
+          cookieStore.set({ name, value: "", ...options });
         },
       },
     }
   );
 
   if (token_hash && type) {
-    const { error } = await supabse.auth.verifyOtp({
+    const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
+    console.log({ error });
     if (!error) {
       return NextResponse.redirect(next);
     }
   }
-
   return NextResponse.redirect("/error");
 }
